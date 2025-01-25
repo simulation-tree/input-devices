54167f5ab86cb2e9bea5a53972a9c2b504aa34a4
Phill <philldamaskin@gmail.com>
2025-01-25 16:14:25 -0500

    add test to check if mouse is itself

--- tests/InputDeviceEntityTests.cs
+++ tests/InputDeviceEntityTests.cs
@@ -9,6 +9,17 @@ namespace InputDevices.Tests
         {
             using World world = CreateWorld();
             Keyboard keyboard = new(world);
+
+            Assert.That(keyboard.Is(), Is.True);
+        }
+
+        [Test]
+        public void CheckIfMouseIsItself()
+        {
+            using World world = CreateWorld();
+            Mouse mouse = new(world);
+
+            Assert.That(mouse.Is(), Is.True);
         }
     }
 }
